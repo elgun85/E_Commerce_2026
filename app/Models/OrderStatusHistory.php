9f06997e875679e7b0bedee3b9ffdb340eae8f59
elgun <elgun.huseynov@azeurotel.az>
2026-01-23 15:01:06 +0400

    OrderHistory hazir

--- app/Models/OrderStatusHistory.php
+++ app/Models/OrderStatusHistory.php
@@ -6,7 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderStatusHistory extends Model
 {
-    //
+        protected $fillable = [
+        'order_id',
+        'user_id',
+        'status',
+        'notes',
+    ];
+
+    // Relationships
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     
 }
