4f173c28eb8774596ffe39ef61e183170d15fe86
elgun <elgun.huseynov@azeurotel.az>
2026-01-23 14:59:48 +0400

    Order model hazir

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+        protected $fillable = [
+        'order_id',
+        'product_id',
+        'product_variant_id',
+        'product_name',
+        'product_sku',
+        'variant_name',
+        'price',
+        'quantity',
+        'subtotal',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'quantity' => 'integer',
+        'subtotal' => 'decimal:2',
+    ];
+    // Relationships
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    public function variant()
+    {
+        return $this->belongsTo(ProductVariant::class, 'product_variant_id');
+    }
 }
