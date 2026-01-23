8b53859e5d07d36f8b183d24e58d121fd14967fc
elgun <elgun.huseynov@azeurotel.az>
2026-01-23 12:33:35 +0400

    Address,Brand,category,coupon,couponUsage modeller ici hazirlandi relation elaqeler yazildi

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,9 +2,61 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    protected $fillable = [
+        'customer_id',
+        'full_name',
+        'phone',
+        'address_line_1',
+        'address_line_2',
+        'city',
+        'state',
+        'postal_code',
+        'country',
+        'is_default',
+        'type',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_default' => 'boolean'
+        ];
+    }
+
+    #[Scope()]
+
+    protected function default(Builder $builder)
+    {
+        $builder->where('is_active', true);
+    }
+
+    #[Scope()]
+
+    protected function ofType(Builder $builder, string $type)
+    {
+        $builder->where('type', $type);
+    }
+
+    //relationship
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+        public function getFullAddressAttribute(){
+        return implode(', ',array_filter([
+            $this->address_line_1,
+            $this->address_line_2,
+            $this->city,
+            $this->state,
+            $this->postal_code,
+            $this->country,
+        ]));
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,20 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use League\Uri\Builder;
+use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
@@ -23,6 +30,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'is_active'
+
     ];
 
     /**
@@ -47,6 +57,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_active' => 'boolean',
         ];
     }
 
@@ -58,7 +69,29 @@ class User extends Authenticatable
         return Str::of($this->name)
             ->explode(' ')
             ->take(2)
-            ->map(fn ($word) => Str::substr($word, 0, 1))
+            ->map(fn($word) => Str::substr($word, 0, 1))
             ->implode('');
+    }
+
+
+    //local scopes
+
+    #[Scope()]
+    public function active(EloquentBuilder $builder)
+    {
+        return $builder->where('is_active', true);
+    }
+
+    //
+
+    public function orderStatusHistories()
+    {
+        return $this->hasMany(OrderStatusHistory::class);
+    }
+
+    //Bu metod Filament panel-ə KİMİN daxil ola biləcəyini müəyyən edir.
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@examble.com');
     }
 }
